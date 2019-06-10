3541602505b20b7293e4b3157f1a77802036d9a5
NaremanPro <violette1988@gmail.com>
2019-06-10 22:54:44 +0300

    updating courses table and insert data

--- src/dataBase/db_build.sql
+++ src/dataBase/db_build.sql
@@ -7,13 +7,17 @@ DROP TABLE IF EXISTS participators CASCADE;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   user_name VARCHAR(30) NOT NULL,
-  user_email VARCHAR(100) NOT NULL,
+  user_email VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE courses (
   course_id SERIAL PRIMARY KEY,
-  course_name VARCHAR(100) NOT NULL
+  course_name VARCHAR(100) NOT NULL,
+  timewhen VARCHAR(100) NOT NULL,
+  place VARCHAR(100) NOT NULL,
+  age VARCHAR(50) NOT NULL,
+  cost VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE participators (
@@ -23,5 +27,14 @@ CREATE TABLE participators (
 
 INSERT INTO users (user_name, user_email, password) VALUES
 ('example', 'example@example.com', 'example123');
+
+INSERT INTO courses (course_name, timewhen, place, age, cost) VALUES
+('Arabic Calligraphy', '1 July 2019, On Monday At 17:00-20:00', 'Lotus Space - Isfiya', '20-30 years old', '450 NIS');
+INSERT INTO courses (course_name, timewhen, place, age, cost) VALUES
+('Mozaik Art', '2 July 2019, On Tuesday At 17:00-20:00', 'Lotus Space - Isfiya', '15-25 years old', '500 NIS');
+INSERT INTO courses (course_name, timewhen, place, age, cost) VALUES
+('Charcoal Art', '3 July 2019, On Wednesday At 17:00-20:00', 'Lotus Space - Isfiya', '15-30 years old', '400 NIS');
+INSERT INTO courses (course_name, timewhen, place, age, cost) VALUES
+('Mandala Art', '4 July 2019, On Thursday At 17:00-20:00', 'Lotus Space - Isfiya', '20-30 years old', '300 NIS');
 
 COMMIT;
